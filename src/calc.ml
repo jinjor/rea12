92e8539d9b367688e760eadf7745ec7a121d2f76
鳥居 陽介 <jinjor@torii-no-MacBook-Air.local>
2013-11-09 20:34:31 +0900

    output to file

--- src/calc.ml
+++ src/calc.ml
@@ -6,18 +6,41 @@ let load_file f =
   close_in ic;
   (s)
 
-let _ =
-  let dir = Sys.getcwd() in
-  let source = load_file (dir ^ "/sample/sample1.alt") in
-  print_string (source ^ "\n");
+let log s =
+  print_string s;
+  print_newline()
+
+let make_js s =
+  string_of_int s
+
+exception Unknown
+let write_file file_name s : unit =
+  let out = open_out file_name in
   try
+    output_string out s;
+  with
+  | _ -> print_string ("error while writing file" ^ file_name);
+  close_out out
+
+let _ =
+  try
+    let dir = Sys.getcwd() in
+    let source_name = "sample1" in
+    let input_file_name = dir ^ "/sample/" ^ source_name ^ ".alt" in
+    let output_file_name = dir ^ "/sample/" ^ source_name ^ ".js" in
+
+    let source = load_file input_file_name in
+    log source;
     let lexbuf = Lexing.from_string source in
-    print_string "start!\n";
+    log "start!";
     let result = Parser.main Lexer.token lexbuf in
-    print_int result;
-    print_newline();
-    print_string "end!\n";
+    let output_str = make_js result in
+    log output_str;
+    write_file output_file_name output_str;
+    log "end!";
     exit 0
   with _ ->
-    print_string "error!\n";
+    log "error!";
     exit 1
+
+
