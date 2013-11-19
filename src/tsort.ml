49d018470a8b7a96fdd6fbf4b966257949047b39
jinjor <jinjorweb@gmail.com>
2013-11-20 03:22:25 +0900

    indent

--- src/tsort.ml
+++ src/tsort.ml
@@ -19,14 +19,14 @@ let rec visit (Node (a, parents)) visited l get_node =
 	    | None -> raise (Module_not_found parent)
 	  in
 	  let (visited__, l_) = List.fold_left visit_each_parent (visited_, l) parents in
-      (visited__, (a :: l_))
+    (visited__, (a :: l_))
 	else (visited, l)
 
 let exec (all : (_ node) list) =
 	let get_node = fun name ->
 	  try
 	    let found = List.find (fun (Node (_name, _)) -> name = _name) all in
-		Some found
+	    Some found
 	  with
 	  | Not_found -> None
 	in
